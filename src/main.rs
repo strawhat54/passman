bdbdd169ff4db7a4f62b4b807b6c95d8a0fb4991
strawhat54 <strawhat54x@gmail.com>
2021-06-25 21:57:13 +0530

    idk what the fuck is going on

--- src/main.rs
+++ src/main.rs
@@ -11,34 +11,38 @@ mod manager;
 use bincode;
 use dirs::home_dir;
 use serde::{Deserialize, Serialize};
+use serde_json;
 use std::collections::HashMap;
-use std::env;
+use std::{env, fs, io::Write};
 
-struct Item {
-    name: String,
-    desc: String,
-    salt: Vec<u8>,
-    // created: cration time
-    // date: last update time
-}
+use manager::Item;
 
 type Table = HashMap<Item, bool>;
 static HELP: &str = "HELP MESSAGE";
 
 fn perform(query: &str) {
-    let home = home_dir()
-        .expect("Home folder not found!")
-        .join(".passman.json");
+    let home = home_dir().expect("Home folder not found!");
 
-    let init = home.is_file();
+    let config = home.join(".passman.json");
+    let secret = home.join(".passman_key.json");
 
-    let value = match query {
+    let init = secret.is_file();
+    println!("OK (perform)");
+    println!("{} {}", query == "new", init);
+    match query {
         "new" => {
-            if init {
+            print!("OK (query)");
+            if init == true {
                 panic!("Looks like yout already have initialized passman. You can try other commands or run `passman destroy` to remove the current passwors and start from scratch");
+            } else {
+                let val = manager::new();
+                let json = serde_json::to_string(&val).unwrap();
+                fs::write(&secret, json);
+                let packed = fs::read_to_string(&secret).unwrap();
+                let v: Vec<u8> = serde_json::from_str(&packed).unwrap();
+
+                println!("{:?}", v);
             }
-            // Store master Key
-            // DO SOMETHING
         }
 
         _ => {
@@ -49,20 +53,24 @@ fn perform(query: &str) {
             // AUTHENTICATE
 
             match query {
+                "destroy" => {
+                    unimplemented!();
+                }
+
                 "add" => {
-                    unimplemented!()
+                    unimplemented!();
                 }
                 "update" => {
-                    unimplemented!()
+                    unimplemented!();
                 }
                 "list" => {
-                    unimplemented!()
+                    unimplemented!();
                 }
                 "del" => {
-                    unimplemented!()
+                    unimplemented!();
                 }
                 "info" => {
-                    unimplemented!()
+                    unimplemented!();
                 }
                 _ => {
                     print!("{}", HELP);
